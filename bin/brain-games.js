1738b254df9001c5a7a8c73173f5e41e5ef8cbea
Kirill <kir_nef@inbox.ru>
2023-02-01 14:47:52 +0300

    3 stage

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,5 @@
 #!/usr/bin/env node
+import {que} from '../src/cli.js'
+
 console.log ('Welcome to the Brain Games!');
+que();
---
+++ src/cli.js
@@ -0,0 +1,13 @@
+
+import readlineSync from 'readline-sync';
+
+
+
+export const  que = () => {
+
+const UserName = readlineSync.question('May I have your name? ');
+console.log('Hello, ' , UserName);
+
+};
+
+
